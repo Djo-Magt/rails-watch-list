65965047913c6ac43cd2e2d61bb90120cce5c262
Jonathan Magnet <j.magnet@outlook.com>
2023-06-27 11:44:29 +0200

    ajout du controller lists

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,32 @@
+class ListsController < ApplicationController
+
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+    @bookmark = Bookmark.new
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(set_params)
+      if @list.save
+        redirect_to lists_path, notice: "League was successfully created."
+      else
+        render :new, status: :unprocessable_entity
+      end
+  end
+
+  private
+
+  def set_params
+    params.require(:list).permit(:name)
+  end
+
+end
